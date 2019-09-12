064db37be20cf8779e1a61e8b800ec3eba77c088
lxz1104 <1366499939@qq.com>
2019-09-12 23:47:38 +0800

    更新安装脚本

--- install.sh
+++ install.sh
@@ -1,14 +1,71 @@
 #!/bin/bash
 echo 'Installing...'
 
-mkdir -p ~/ms-dos
+# 安装软件
+Install_DosBox()
+{
+  # Check OS
+    if grep -Eqii "CentOS" /etc/issue || grep -Eq "CentOS" /etc/*-release; then
+        DISTRO='CentOS'
+        PM='yum'
+    elif grep -Eqi "Red Hat Enterprise Linux Server" /etc/issue || grep -Eq "Red Hat Enterprise Linux Server" /etc/*-release; then
+        DISTRO='RHEL'
+        PM='yum'
+    elif grep -Eqi "Aliyun" /etc/issue || grep -Eq "Aliyun" /etc/*-release; then
+        DISTRO='Aliyun'
+        PM='yum'
+    elif grep -Eqi "Fedora" /etc/issue || grep -Eq "Fedora" /etc/*-release; then
+        DISTRO='Fedora'
+        PM='yum'
+    elif grep -Eqi "Debian" /etc/issue || grep -Eq "Debian" /etc/*-release; then
+        DISTRO='Debian'
+        PM='apt'
+    elif grep -Eqi "Ubuntu" /etc/issue || grep -Eq "Ubuntu" /etc/*-release; then
+        DISTRO='Ubuntu'
+        PM='apt'
+    elif grep -Eqi "Raspbian" /etc/issue || grep -Eq "Raspbian" /etc/*-release; then
+        DISTRO='Raspbian'
+        PM='apt'
+    elif grep -Eqi "Manjaro" /etc/issue || grep -Eq "Manjaro" /etc/*-release; then
+        DISTRO='Manjaro'
+        PM='pacman'
+    elif grep -Eqi "Arch" /etc/issue || grep -Eq "Arch" /etc/*-release; then
+        DISTRO='Arch'
+        PM='pacman'
+    else
+        DISTRO='Unknow'
+    fi
+    echo 'OS:' $DISTRO;
+    
+    if [ $DISTRO == 'Unknow' ];
+    then
+      echo '[Warning]: Please install DosBox by yourself!'
+    fi
 
-cp -rf ./ASM ~/ms-dos/
-cp -rf ./FILE ~/ms-dos/ 
-cp -rf ./MASM ~/ms-dos/ 
-cp -rf ./VIM ~/ms-dos/ 
+    echo 'Installing DosBox ...'
+    if [ $PM == 'pacman'  ];
+    then
+      sudo $PM -Sy dosbox
+    else
+      sudo $PM update
+      sudo $PM install dosbox -y
+    fi
+}
 
-cp -f ~/.dosbox/dosbox-0.74.conf ./config/dosbox.config.bak
-cp -f ./config/dosbox.conf ~/.dosbox/dosbox-0.74.conf
+# install config
+Install_Dependent(){
+  mkdir -p ~/ms-dos
 
-echo 'Success!'
+  cp -rf ./ASM ~/ms-dos/
+  cp -rf ./FILE ~/ms-dos/ 
+  cp -rf ./MASM ~/ms-dos/ 
+  cp -rf ./VIM ~/ms-dos/ 
+
+  cp -f ~/.dosbox/dosbox-0.74.conf ./config/dosbox.config.bak
+  cp -f ./config/dosbox.conf ~/.dosbox/dosbox-0.74.conf
+}
+
+Install_DosBox
+Install_Dependent
+
+echo 'Install Success!'
--- uninstall.sh
+++ uninstall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-rm -rf ./ms-dos
+rm -rf ~/ms-dos
 
 cp -f ./config/dosbox.config.bak ~/.dosbox/dosbox-0.74.conf
 
